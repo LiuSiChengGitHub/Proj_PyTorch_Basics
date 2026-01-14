b1010c8d03c2756c3e59d2a14ecdc065dec69c67
LiuSiCheng <liusichengyouxiang@163.com>
2026-01-14 20:36:03 +0800

    call it a day 0114

---
+++ examples/study_nn_layers.py
@@ -0,0 +1,17 @@
+# 用于测试卷积、池化等组件
+
+import torch
+from torch import nn
+
+class newNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self,input):
+        output = input +1
+        return output
+
+newNN1 = newNN()
+x = torch.tensor(1.0)
+output = newNN1(x)
+print(output)
